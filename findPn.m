442a94625f57283a7ca4878aa368f96b6f2bfc0d
aaronfairchild <107432584+aaronfairchild@users.noreply.github.com>
2025-03-01 16:04:52 -0700

    updated findPn

--- findPn.m
+++ findPn.m
@@ -1,6 +1,13 @@
 function [Pn, Pnc, Pns] = findPn(section, materials, reinforcement, analysis)
 
-c = analysis.start_c
+c = analysis.start_c;
+Pn_target = analysis.P;
+tolP = analysis.P_tolerance;
 CPolys = findPolys(section, c);
+
+while err > tol
+    
+    err = abs(Pn - Pn_target)/Pn_target;
+end
 
 end
